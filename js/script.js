ab1257536928c5d7e7036ab38e65a7172911b48b
jwh704 <116139551+jwh704@users.noreply.github.com>
2022-11-30 10:51:08 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,137 +1,113 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
-
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
-  });
-
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
-
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
+//下面这一段全部来自于hexo-theme-nexmoe的部分源码，感谢大佬，指路: https://github.com/theme-nexmoe/hexo-theme-nexmoe
+const getRealPath = (pathname, desc = false) => {
+    if (!pathname) {
+      pathname = window.location.pathname;
+    }
+    let names = pathname.split("/");
+    if (desc === false) {
+      for (let i = names.length - 1; i >= 0; --i) {
+        let name = names[i].trim();
+        if (name.length > 0 && name !== "/" && name !== "index.html") {
+          return name;
+        }
       }
     } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
+      for (let i = 0; i < names.length; ++i) {
+        let name = names[i].trim();
+        if (name.length > 0 && name !== "/" && name !== "index.html") {
+          return name;
+        }
+      }
     }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
-
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
-      var alt = this.alt;
-
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
-    });
-
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
-
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
-
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
+    return "/";
   };
 
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
+let links = document.querySelectorAll(".nav-link");
+let rootRealPath = getRealPath(window.location.pathname, true);
+for (let link of links) {
+  let linkPath = link.getAttribute("href");
+  if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
+    link.className = "active-link nav-link";
   }
+}
+// mobile-nav-link
+let mlinks = document.querySelectorAll(".mobile-nav-link");
+let mrootRealPath = getRealPath(window.location.pathname, true);
+for (let mlink of mlinks) {
+  let mlinkPath = mlink.getAttribute("href");
+  if (mlinkPath && getRealPath(mlinkPath, true) === mrootRealPath) {
+    mlink.className = "moibile-active-link mobile-nav-link";
+  }
+}
 
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
+//设置收起的menu点击的效果
+var mmenu =document.getElementById("mobile-menu");
+var mmain=document.getElementById("mobile-main");
+var mclose=document.getElementById("mobile-close");
 
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
+mmenu.onclick=function(){
+  mmain.style.display="block";
+}
+mclose.onclick=function(){
+  mmain.style.display="none";
+}
 
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
 
-    $container.removeClass('mobile-nav-on');
-  });
-})(jQuery);
+///////////////////////////////////////////////////////////////////////////////////
+
+//设置sort的子目录隐藏显示效果
+if(document.getElementById("sort")){
+var sort=document.getElementById("sort");
+var sortdiv=document.getElementById("sort-div");
+
+sort.onmouseover=function(){
+  sortdiv.style.display="block";
+}
+sort.onmouseout=function(){
+sortdiv.onmouseover=function(){
+  console.log("wuhu");
+}
+sortdiv.onmouseout=function(){
+    sortdiv.style.display="none";
+}
+}
+}
+
+//图片懒加载
+var imgs = document.querySelectorAll('img');
+var imgdivs = document.querySelectorAll('.lazyload-img-span');
+
+
+//用来判断bound.top<=clientHeight的函数，返回一个bool值
+function isIn(el) {
+    var bound = el.getBoundingClientRect();
+    var clientHeight = window.innerHeight;
+    return bound.top <= clientHeight;
+} 
+//检查图片是否在可视区内，如果在，则加载
+function check() {
+  for(let p=0 ; p<imgs.length;p++){
+    if(isIn(imgs[p])){
+        loadImg(imgs[p]);
+        imgs[p].onload = function(){
+            let y=Number(p);
+            changeClass(y);
+        }
+    }
+}
+}
+function changeClass(num){
+  let tempse=Number(num);
+  imgdivs[tempse].className = "lazyload-img-span img-masks";
+}
+function loadImg(el) {
+    if(!el.src){
+        var source = el.dataset.src;
+        el.src = source;
+    }
+}
+
+window.onload = window.onscroll = function () { //onscroll()在滚动条滚动的时候触发
+    check();
+}
